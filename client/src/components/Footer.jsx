8c4c698140bed6b2493c54f9177dfa5abcac2caf
sant123-crypto680 <146374339+sant123-crypto680@users.noreply.github.com>
2025-08-12 13:45:01 +0530

    Update Footer.jsx
    update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
                 <div className="md:max-w-96">
                     <img className="w-36 h-auto" src={assets.logo} alt="logo" />
                     <p className="mt-6 text-sm">
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                       Book your show now !! 
                     </p>
-                    <div className="flex items-center gap-2 mt-4">
+{/*                     <div className="flex items-center gap-2 mt-4">
                         <img src={assets.googlePlay} alt="google play" className="h-9 w-auto" />
                         <img src={assets.appStore} alt="app store" className="h-9 w-auto" />
-                    </div>
+                    </div> */}
                 </div>
                 <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
                     <div>
@@ -28,17 +28,18 @@ const Footer = () => {
                     <div>
                         <h2 className="font-semibold mb-5">Get in touch</h2>
                         <div className="text-sm space-y-2">
-                            <p>+1-234-567-890</p>
-                            <p>contact@example.com</p>
+                           <p>+91 8434296662</p>
+                            <p>sk30205881550@gmail.com</p>
                         </div>
                     </div>
                 </div>
             </div>
             <p className="pt-4 text-center text-sm pb-5">
-                Copyright {new Date().getFullYear()} © GreatStack. All Right Reserved.
+                Copyright {new Date().getFullYear()} © santosh . All Right Reserved.
             </p>
         </footer>
   )
 }
 
 export default Footer
+
